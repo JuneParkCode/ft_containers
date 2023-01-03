c8bc05532708cfd6a53d971dcd41a6172d619582
Photogrammer <khsj15@khu.ac.kr>
2023-01-03 10:45:09 +0900

    refactor : align

--- source/vector/vector.hpp
+++ source/vector/vector.hpp
@@ -1,14 +1,10 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-#include <cstddef>
-#include <cstdint>
 #include <exception>
 #include <iterator>
 #include <memory>
-#include <stdexcept>
-#include <sys/_types/_size_t.h>
-#include <vector>
+#include "ft_iterator.hpp"
 
 namespace ft
 {
@@ -17,21 +13,25 @@ namespace ft
 	{
 		public:
 			/** type define */
-			typedef size_t										size_type;
-			typedef T											value_type;
-			typedef Allocator									allocator_type;
+			typedef size_t		size_type;
+			typedef T			value_type;
+			typedef Allocator	allocator_type;
 			/** type define related with allocator */
 			typedef typename allocator_type::reference			reference;
 			typedef typename allocator_type::const_reference	const_reference;
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
 			typedef std::allocator_traits<allocator_type>		__alloc_traits;
+			/** iterator types */
+			typedef typename std::iterator<std::random_access_iterator_tag, value_type>	iterator;
+			typedef typename ft::reverse_iterator<iterator>								reverse_iterator;
+
 		/** member variables  */
 		private:
-			pointer 											mData;
-			size_type											mDataStorageSize;
-			size_type											mDataStoreSize; 
-			const Allocator&									mAllocator;
+			pointer 			mData;
+			size_type			mDataStorageSize;
+			size_type			mDataStoreSize; 
+			const Allocator&	mAllocator;
 		/* constructor. destructors */
 		public:
 			explicit vector(const Allocator& allocator = std::allocator<T>()):
@@ -72,197 +72,17 @@ namespace ft
 				for (size_type idx = 0; idx < rhs.size(); ++idx)
 					this->push_back(rhs[idx]);
 			}
-			allocator_type get_allocator() const { return (mAllocator); }
+			allocator_type get_allocator() const		{ return (mAllocator); }
 		/* iterators */
-		public:
-			class iterator
-			{
-				typedef std::random_access_iterator_tag	iterator_category;
-				typedef std::ptrdiff_t					difference_type;
-				typedef T								value_type;
-				typedef pointer							pointer_type;
-				typedef reference						reference_type;
-			private:
-				pointer_type mPtr;
-			public:
-				iterator(pointer_type ptr): mPtr(ptr) {}
-				reference_type operator*()  { return (*mPtr); }
-				pointer_type operator->() { return (mPtr); }
-				iterator& operator++()
-				{
-					++mPtr;
-					return (*this);
-				}
-				iterator& operator++(int)
-				{
-					iterator it = *this;
-					++mPtr;
-					return (it);
-				}
-				iterator& operator--()
-				{
-					--mPtr;
-					return (*this);
-				}
-				iterator& operator--(int)
-				{
-					iterator it = *this;
-					--mPtr;
-					return (it);
-				}
-				iterator& operator=(const iterator& it)
-				{
-					iterator temp(it.mPtr);
-					return (temp);
-				}
-				bool operator==(const iterator& it) { return (mPtr == it.mPtr); }
-				bool operator!=(const iterator& it) { return (mPtr != it.mPtr); }
-			};
-
-			class const_iterator
-			{
-				typedef std::random_access_iterator_tag	iterator_category;
-				typedef std::ptrdiff_t					difference_type;
-				typedef T								value_type;
-				typedef pointer							pointer_type;
-				typedef reference						reference_type;
-			private:
-				pointer_type mPtr;
-			public:
-				const_iterator(pointer_type ptr): mPtr(ptr) {}
-				const value_type& operator*()  { return (*mPtr); }
-				const value_type* operator->() { return (mPtr); }
-				const_iterator& operator++()
-				{
-					++mPtr;
-					return (*this);
-				}
-				const_iterator& operator++(int)
-				{
-					const_iterator it = *this;
-					++mPtr;
-					return (it);
-				}
-				const_iterator& operator--()
-				{
-					--mPtr;
-					return (*this);
-				}
-				const_iterator& operator--(int)
-				{
-					const_iterator it = *this;
-					--mPtr;
-					return (it);
-				}
-				const_iterator& operator=(const const_iterator& it)
-				{
-					const_iterator temp(it.mPtr);
-					return (temp);
-				}
-				bool operator==(const const_iterator& it) { return (mPtr == it.mPtr); }
-				bool operator!=(const const_iterator& it) { return (mPtr != it.mPtr); }
-			};
-			/** FIXME: reverse iterator uses std::reverse_iterator<T>  */
-			class reverse_iterator
-			{
-				typedef std::random_access_iterator_tag	iterator_category;
-				typedef std::ptrdiff_t					difference_type;
-				typedef T								value_type;
-				typedef pointer							pointer_type;
-				typedef reference						reference_type;
-			private:
-				pointer_type mPtr;
-			public:
-				reverse_iterator(pointer_type ptr): mPtr(ptr) {}
-				reference_type operator*()  { return (*mPtr); }
-				pointer_type operator->() { return (mPtr); }
-				reverse_iterator& operator++()
-				{
-					--mPtr;
-					return (*this);
-				}
-				reverse_iterator& operator++(int)
-				{
-					reverse_iterator it = *this;
-					--mPtr;
-					return (it);
-				}
-				reverse_iterator& operator--()
-				{
-					++mPtr;
-					return (*this);
-				}
-				reverse_iterator& operator--(int)
-				{
-					reverse_iterator it = *this;
-					++mPtr;
-					return (it);
-				}
-				reverse_iterator& operator=(const reverse_iterator& it)
-				{
-					reverse_iterator temp(it.mPtr);
-					return (temp);
-				}
-				bool operator==(const reverse_iterator& it) { return (mPtr == it.mPtr); }
-				bool operator!=(const reverse_iterator& it) { return (mPtr != it.mPtr); }
-			};
-
-			class const_reverse_iterator
-			{
-				typedef std::random_access_iterator_tag	iterator_category;
-				typedef std::ptrdiff_t					difference_type;
-				typedef T								value_type;
-				typedef pointer							pointer_type;
-				typedef reference						reference_type;
-			private:
-				pointer_type mPtr;
-			public:
-				const_reverse_iterator(pointer_type ptr): mPtr(ptr) {}
-				const value_type& operator*()  { return (*mPtr); }
-				const value_type* operator->() { return (mPtr); }
-				const_reverse_iterator& operator++()
-				{
-					--mPtr;
-					return (*this);
-				}
-				const_reverse_iterator& operator++(int)
-				{
-					const_reverse_iterator it = *this;
-					--mPtr;
-					return (it);
-				}
-				const_reverse_iterator& operator--()
-				{
-					++mPtr;
-					return (*this);
-				}
-				const_reverse_iterator& operator--(int)
-				{
-					const_reverse_iterator it = *this;
-					++mPtr;
-					return (it);
-				}
-				const_reverse_iterator& operator=(const const_reverse_iterator& it)
-				{
-					const_reverse_iterator temp(it.mPtr);
-					return (temp);
-				}
-				bool operator==(const const_reverse_iterator& it) { return (mPtr == it.mPtr); }
-				bool operator!=(const const_reverse_iterator& it) { return (mPtr != it.mPtr); }
-			};
-			iterator				begin() { return (iterator(&mData[0])); }
-			iterator				end() { return (iterator(&mData[mDataStorageSize])); }
-			const_iterator			cbegin() const { return (const_iterator(&mData[0])); }
-			const_iterator			cend() const { return (const_iterator(&mData[mDataStorageSize])); }
-			reverse_iterator		rbegin() { return (reverse_iterator(&mData[mDataStorageSize - 1])); }
-			reverse_iterator		rend() { return (reverse_iterator(&mData[-1])); }
-			const_reverse_iterator	crbegin() const { return (const_reverse_iterator(&mData[mDataStorageSize - 1])); }
-			const_reverse_iterator	crend() const { return (const_reverse_iterator(&mData[-1])); }
+			iterator				begin()				{ return (iterator(&mData[0])); }
+			iterator				end()				{ return (iterator(&mData[mDataStorageSize])); }
+			reverse_iterator		rbegin()			{ return (reverse_iterator(end())); }
+			reverse_iterator		rend()				{ return (reverse_iterator(begin())); }
 		/** member functions */
 		public:
 			/** capacity  */
-			size_type				size() const { return (mDataStoreSize); }
-			size_type				max_size() const { return (INT32_MAX); }
+			size_type				size() const		{ return (mDataStoreSize); }
+			size_type				max_size() const	{ return (INT32_MAX); }
 			void					resize(size_type n, value_type val = value_type()) // it destroy stored data
 			{
 				if (n < this->mDataStoreSize)
@@ -299,8 +119,8 @@ namespace ft
 					}
 				}
 			}
-			size_type				capacity() const { return (mDataStorageSize); }
-			bool					empty() const { return (mDataStoreSize == 0); }
+			size_type				capacity() const			{ return (mDataStorageSize); }
+			bool					empty() const				{ return (mDataStoreSize == 0); }
 			void					reserve(size_type n)
 			{
 				if (n > mDataStorageSize)
@@ -315,7 +135,7 @@ namespace ft
 			}
 			
 			/** element access */
-			reference				operator[](int idx) const { return (mData[idx]); }
+			reference				operator[](int idx) const	{ return (mData[idx]); }
 			reference				at(size_type n)
 			{
 				if (n > mDataStoreSize || n < 0)
@@ -328,11 +148,11 @@ namespace ft
 					throw (std::out_of_range("out of range : at()"));
 				return (mData[n]);
 			}
-			reference				front() { return (mData[0]); }
-			const reference			front() const { return (mData[0]); }
-			reference				back() { return (mData[mDataStoreSize - 1]); }
-			const reference			back() const { return (mData[mDataStoreSize - 1]); }
-			const pointer			data() const { return (mData); }
+			reference				front()						 { return (mData[0]); }
+			const reference			front() const				 { return (mData[0]); }
+			reference				back()						 { return (mData[mDataStoreSize - 1]); }
+			const reference			back() const				 { return (mData[mDataStoreSize - 1]); }
+			const pointer			data() const				 { return (mData); }
 			
 			/** modifiers */
 			template <class InputIterator>
